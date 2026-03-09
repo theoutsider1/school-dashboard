94ca42d7f6064768209c1bd62f90ee90d60133c4
Hatim Tagmi <hatim.tagmi@ef2conseil.com>
2026-03-09 00:43:51 +0000

    chore: add CSRF token to window object in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- CSRF token for JS -->
+        <script>
+            window.csrfToken = '{{ csrf_token() }}';
+        </script>
+        
         <!-- Scripts -->
         @routes
         @viteReactRefresh
