43ee5a49d8b53c0b7b554ab7485fa77bf54f4b09
Hatim Tagmi <hatim.tagmi@ef2conseil.com>
2026-03-10 23:44:08 +0000

    feat: add index method in ClasseController to list all classes

---
+++ app/Http/Controllers/ClasseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Classe; 
+
+class ClasseController extends Controller
+{
+    //Show All Classes
+    public function index () 
+    {
+        $classes = Classe::all(); // pagination later
+        return Inertia::render('Classes/Index', [
+            'classes' => $classes
+        ]);
+    }
+}
