20033eee6852965f6cead66cf0f5709cf1090474
Hatim Tagmi <hatim.tagmi@ef2conseil.com>
2026-03-10 23:45:13 +0000

    chore: add necessary imports to Classe model

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Support\Facades\Auth;
+use App\Models\User; 
+use App\Models\Student; 
 class Classe extends Model
 {
     //
@@ -50,7 +53,7 @@ class Classe extends Model
 
     public function students ()
     {
-        return $this-hasMany(Student::class, 'class_id');
+        return $this->hasMany(Student::class, 'class_id');
     }
 
 
